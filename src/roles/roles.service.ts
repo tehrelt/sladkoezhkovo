f38e031b1a4f14017351e08e0e0e151520a76633
Dmitry Evteev <haxrelt@gmail.com>
2024-04-23 15:34:01 +0300

    role service logger

--- src/roles/roles.service.ts
+++ src/roles/roles.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { CreateRoleDto } from './dto/create-role.dto';
 import { uuidv7 } from 'uuidv7';
 import { PrismaService } from 'src/prisma/prisma.service';
@@ -6,10 +6,13 @@ import { isUUID } from 'class-validator';
 
 @Injectable()
 export class RolesService {
+  private readonly logger = new Logger('RolesService');
+
   constructor(private readonly prisma: PrismaService) {}
 
   async create(dto: CreateRoleDto) {
     const id = uuidv7();
+    this.logger.debug(`creating role id=${id} dto=${JSON.stringify(dto)}`);
     return await this.prisma.role.create({
       data: {
         id: id,
@@ -24,6 +27,7 @@ export class RolesService {
   }
 
   findOne(slug: string) {
+    this.logger.debug(`looking for role with slug '${slug}'`);
     const role = isUUID(slug)
       ? this.prisma.role.findUnique({ where: { id: slug } })
       : this.prisma.role.findUnique({ where: { name: slug } });
@@ -32,6 +36,7 @@ export class RolesService {
   }
 
   async remove(slug: string) {
+    this.logger.debug('deleting role with slug' + slug);
     return isUUID(slug)
       ? await this.prisma.role.delete({ where: { id: slug } })
       : await this.prisma.role.delete({ where: { name: slug } });
