d527e2040371f1cfef74710f38a1b7f30969e0e6
Dmitry Evteev <haxrelt@gmail.com>
2024-04-23 13:01:02 +0300

    config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from './prisma/prisma.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, ConfigModule.forRoot({ envFilePath: '.env' })],
   controllers: [],
   providers: [],
 })
